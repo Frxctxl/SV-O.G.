9dcbbb932fb080c6cc6768410f1923dad253f565
Jayden Benston <jayden@Jaydens-Laptop.local>
2024-07-20 09:02:11 -0400

    finished prompt functionality

---
+++ index.js
@@ -0,0 +1,15 @@
+// import statement for logo prompt
+const logoPrompt = require('./lib/prompt');
+function init() {
+  //welcome method
+  console.log(`
+----------------------
+  Welcome to SV O.G!
+----------------------
+`)
+
+  //main menu prompt
+  logoPrompt();
+}
+
+init();
